78fd8f0569c9f417df20e03d86ba2a242d7bc246
Alexandra <sunny.belyavskaya@gmail.com>
2020-02-17 21:20:14 +0300

    constructor, state, ref

--- src/App.js
+++ src/App.js
@@ -5,21 +5,46 @@ import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
-    tasks=[
-    {title: "CSS", isDone: true, priority: "low"},
-    {title: "JS", isDone: false, priority: "medium"},
-    {title: "ReactJS", isDone: true, priority: "high"},
-    {title: "Patterns", isDone: true, priority: "medium"}
-];
-    filterValue="Active";
+    constructor(props){
+        super(props);
+        this.newTaskTitleRef=React.createRef();
+    }
+
+    state={
+        tasks:[
+            {title: "CSS", isDone: true, priority: "low"},
+            {title: "JS", isDone: false, priority: "medium"},
+            {title: "ReactJS", isDone: true, priority: "high"},
+            {title: "Patterns", isDone: true, priority: "medium"}
+        ],
+        filterValue: "Active",
+    }
+
+    onAddTaskClick=()=>{
+        if(this.newTaskTitleRef.current.value!==""){
+            let newText=this.newTaskTitleRef.current.value;
+            this.newTaskTitleRef.current.value="";
+            let newTask= {title: newText,
+                isDone: true,
+                priority: "low"};
+            let newTasks=[...this.state.tasks, newTask];
+            this.setState({
+                    tasks: newTasks,
+                }
+            )
+        }
+        else alert("Введите заголовок!");
+
+    }
+
     render = () => {
 
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader/>
-                    <TodoListTasks tasks={this.tasks}/>
-                    <TodoListFooter filterValue={this.filterValue}/>
+                    <TodoListHeader onAddTaskClick={this.onAddTaskClick} titleRef={this.newTaskTitleRef} />
+                    <TodoListTasks tasks={this.state.tasks}/>
+                    <TodoListFooter filterValue={this.state.filterValue}/>
                 </div>
             </div>
         );
